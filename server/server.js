152a85419ae3799d14578efb0902dbd3963281c3
AnniePriya <anniepriya.24it@licet.ac.in>
2026-02-13 13:03:58 +0530

    update

--- server/server.js
+++ server/server.js
@@ -112,7 +112,7 @@ app.post("/api/save-love-note", async (req, res) => {
 /* -------------------------------
    START SERVER
 --------------------------------*/
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
--- src/pages/Games/RatingGame.jsx
+++ src/pages/Games/RatingGame.jsx
@@ -35,9 +35,7 @@ export default function RatingGame() {
     } else {
       // 🔥 SAVE TO DATABASE
       try {
-        await axios.post(
-          `${import.meta.env.VITE_API_URL}/api/save`,
-          {
+        await axios.post(`${import.meta.env.VITE_API_URL}/api/save`,{
           username: "jerosh",
           round: "ratingGame",
           clickAnswers: updatedRatings.map(String), // convert to string
